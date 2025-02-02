d3bc43c29f89bd7c2366399e095c3ff74e9049cb
Blaine <blaine.oania@gmail.com>
2025-02-02 10:14:33 -0500

    safety push

--- src/app/components/SignInForm.jsx
+++ src/app/components/SignInForm.jsx
@@ -1,3 +1,4 @@
+// components/SignInForm.jsx
 'use client';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
--- src/app/pages/main.js
+++ src/app/pages/main.js
@@ -2,12 +2,15 @@
 import AddDeviceForm from '../components/AddDeviceForm';
 import DevicesList from '../components/DevicesList';
 import SignOutButton from '../components/SignOutButton';
+import styles from '../styles/MainPage.module.css'; // Import CSS module for main page
 
 export default function Main() {
   return (
-    <div>
-      <h1>Main Page</h1>
-      <SignOutButton />
+    <div className={styles.mainPage}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Main Page</h1>
+        <SignOutButton />
+      </div>
       <DevicesList />
       <AddDeviceForm />
     </div>
